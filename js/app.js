c2c77d54c4f4cb58b828f799a88bbe53b9e99194
Shunaam <67766966 + Geogith@users.noreply.github.com>
2020-11-23 20:48:46 -0600

    modified code and files

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ var plotHeight = svgHeight - plotMargin.top - plotMargin.bottom;
 
 // Select body/div-id, append SVG area to body of page, and set the dimensions
 var svg = d3
-  .select("svg_scatterplot-area")
+  .select("#scatter")
   .append("svg")
   .attr("height", svgHeight + plotMargin.top + plotMargin.bottom)
   .attr("width", svgWidth + plotMargin.left + plotMargin.right)
@@ -37,41 +37,46 @@ var svg = d3
 
 // console.log(svg);
 
-// var plotGroup = svg.append("g")
-//   .attr("transform", `translate(${plotMargin.left}, ${plottMargin.top})`);
-
 //Read data from censusdata.csv
 // d3.csv(https://data.census.gov/cedsci/table?q=2014%20acs%20health%20insurance%20by%20Income%20and%20Poverty&tid=ACSDT1Y2014.B27015&hidePreview=false [, row, callback]);
 
 //Read and Load data from censusdata.csv
-d3.csv("censusdata.csv").then(function (censusData) {
-  // Print the censusData
-  console.log(censusData);
+d3.csv("./data_resources/data_uhi.csv").then(function (censusData) {
+  // console.log(censusData);
 
-  // Add X axis
-  var x = d3.scaleLinear().domain([0, 4000]).range([0, width]);
-  svg
-    .append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+  // Add X and Y axis
+  var x = d3
+    .scaleLinear()
+    .domain([
+      d3.min(censusData, (d) => d.proverty),
+      d3.max(censusData, (d) => d.proverty)
+    ])
+    .range([0, svgWidth]);
 
-  // Add Y axis
-  var y = d3.scaleLinear().domain([0, 500000]).range([height, 0]);
+  var y = d3
+    .scaleLinear()
+    .domain([0, d3.max(censusData, (d) => d.healthcare)])
+    .range([svgHeight, 0]);
   svg.append("g").call(d3.axisLeft(y));
 
-  // Add dots
   svg
     .append("g")
-    .selectAll("dot")
-    .data(data)
+    .attr("transform", "translate(0," + svgHeight + ")")
+    .call(d3.axisBottom(x));
+
+  // Add circles
+  svg
+    .selectAll("circle")
+    .data(censusData)
     .enter()
     .append("circle")
     .attr("cx", function (d) {
-      return x(d.incomelevel);
+      // console.log(d.state);
+      return x(d.state);
     })
     .attr("cy", function (d) {
-      return y(d.noHI);
+      return y(parseInt(d.healthcare));
     })
-    .attr("r", 1.5)
+    .attr("r", 10)
     .style("fill", "#69b3a2");
 });
